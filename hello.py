54c5276171325db425c9eb4ce4e38b2762ac649d
EmptyBox303 <JasonLu2027@u.northwestern.edu>
2025-06-26 09:13:02 -0400

    pasting in basic code

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
 #this is a basic thing I'm testing out
 #what happens if I add a line here?
 #what about now?
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
